12782413e1c04da34699c824a09a9d06b567f115
Felipe Min <felipemin03@gmail.com>
2025-04-05 14:28:39 -0400

    db fixed

---
+++ api/connection.php
@@ -0,0 +1,15 @@
+<?php
+// this file stablishes a connection to the MySQL database
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "sys";
+
+// Create connection using MySQLi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
